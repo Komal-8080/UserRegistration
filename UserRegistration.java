15886fe1183bd840daf583f1c34ef0f0afa1eee7
Komal-8080 <shindeg163@gmail.com>
2020-12-31 20:02:47 +0530

    UC3: Pattern added to check user Email validity

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,12 +6,12 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
-	public static void firstName() {
-		System.out.println("Enter first Name");
+	public static void FirstName() {
+		System.out.println("Enter First Name");
 		Scanner input = new Scanner(System.in);
-		String entry = input.nextLine();
+		String firstName = input.nextLine();
 		Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2,}$");
-		Matcher match = pattern.matcher(entry); 
+		Matcher match = pattern.matcher(firstName); 
 		boolean patternValidity = match.matches();
 			if (patternValidity == true) {
 				System.out.println("your Entry is Valid");	
@@ -21,12 +21,27 @@ public class UserRegistration {
 			}
 	}
 	
-	public static void lastName() {
+	public static void LastName() {
 		System.out.println("Enter Last Name");
 		Scanner input = new Scanner(System.in);
-		String entry = input.nextLine();
+		String lastName = input.nextLine();
 		Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2,}$");
-		Matcher match = pattern.matcher(entry); 
+		Matcher match = pattern.matcher(lastName); 
+		boolean patternValidity = match.matches();
+			if (patternValidity == true) {
+				System.out.println("your Entry is Valid");	
+			}
+			else {
+				System.out.println("Your Entry is Invalid");
+			}
+	}
+	
+	public static void Email() {
+		System.out.println("Enter your Email");
+		Scanner input = new Scanner(System.in);
+		String email = input.nextLine();
+		Pattern pattern = Pattern.compile("^[a-zA-Z]{3}\\.[a-zA-Z]{1,}@[a-zA-Z]{2}\\.[a-zA-Z]{2}\\.[a-zA-Z]{1,}$");
+		Matcher match = pattern.matcher(email); 
 		boolean patternValidity = match.matches();
 			if (patternValidity == true) {
 				System.out.println("your Entry is Valid");	
@@ -37,7 +52,8 @@ public class UserRegistration {
 	}
 		
 	public static void main(String args[]) {
-		firstName();
-		lastName();
+		FirstName();
+		LastName();
+		Email();
 		}
 }
